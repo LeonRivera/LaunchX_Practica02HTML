6cbbfde024c5be23eaa8caab237ec3c97f69c4f7
LeonRivera <leonriv2@gmail.com>
2022-04-16 23:04:46 -0500

    16042022 | LeonRivera | Show the structure of the menu mobile responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 let btnMenu = document.querySelector('.menu-icon');
+let btnMenuMobile = document.querySelector('.menu-icon-mobile');
+let menuContainer = document.querySelector('.menu-mobile-container');
 
 btnMenu.addEventListener('click', (e) => {
-    console.log("Hola mundo");
+    console.log("click btn menu");
+    menuContainer.classList.add("visibleDisplay");
+});
+btnMenuMobile.addEventListener('click', (e) => {
+    console.log("click btn mobile");
+
+    menuContainer.classList.remove("visibleDisplay");
 });
