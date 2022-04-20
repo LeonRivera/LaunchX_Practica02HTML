4e875282274ad970b57762c1000cfe5221343c25
LeonRivera <leonriv2@gmail.com>
2022-04-19 21:14:22 -0500

    19042022 | LeonRivera | Created fixed menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 let btnMenu = document.querySelector('.menu-icon');
 let btnMenuMobile = document.querySelector('.menu-icon-mobile');
 let menuContainer = document.querySelector('.menu-mobile-container');
+let nav = document.querySelector('nav');
+let footer = document.querySelector('footer');
+// console.log(sections);
 
 btnMenu.addEventListener('click', (e) => {
     console.log("click btn menu");
@@ -16,5 +19,38 @@ btnMenuMobile.addEventListener('click', (e) => {
 
 
 window.addEventListener('DOMContentLoaded', (e) => {
-    console.log("hola");
 });
+
+
+// Observer 
+const options = {
+    // root,
+    // rootMargin: '0px',
+    // threshold: 1.0 
+}
+const callback = (entries, observer) => {
+    console.log("se llamo al callback");
+    entries.forEach(e => {
+        if (!e.isIntersecting) {
+
+            if (!e.target.classList.contains("nav-not")) {
+                e.target.classList.add("nav-not")
+            }
+
+            // }else{
+            //     
+            console.log("intersecting");
+        }
+
+        // if (e.isIntersecting) {
+        //     if (e.target.classList.contains("nav-not")) {
+        //         e.target.classList.remove("nav-not")
+        //     }
+        // }
+
+    });
+}
+
+
+const observer = new IntersectionObserver(callback, options);
+observer.observe(document.querySelector('.navbar'));
